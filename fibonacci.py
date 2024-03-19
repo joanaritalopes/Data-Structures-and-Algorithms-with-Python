22e1c5e70cebe4574c307b3da8f96446b76583b4
joanaritalopes <joana.romeiro@hotmail.com>
2024-03-19 18:16:35 +0100

    fibonacci update

--- fibonacci.py
+++ fibonacci.py
@@ -1,15 +1,14 @@
 # Recursive implemetation
 def fibonacci(n):
-  # base cases
-  if n == 1:
-    return n
-  elif n == 0:
-    return n
-  
-  else: 
-    # recursive step
-    print("Recursive call with {0} as input".format(n))
-    return fibonacci(n - 1) + fibonacci(n - 2)
+    # base cases
+    if n == 1:
+        return n
+    elif n == 0:
+        return n
+    else: 
+        # recursive step
+        print("Recursive call with {0} as input".format(n))
+        return fibonacci(n - 1) + fibonacci(n - 2)
 
 
 #Interactive implementation
@@ -22,4 +21,5 @@ def fibonacci(n):
             next_fib = fib_list[-1] + fib_list[-2]
             fib_list.append(next_fib)
             n -=1
+
     return fib_list[-1]
